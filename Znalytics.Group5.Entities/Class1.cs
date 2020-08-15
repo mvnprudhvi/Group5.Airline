09e1dca0e8968b8fe06c30d0f673cf73987b9303
Madhumitha-Varma <madhumithagorla@gmail.com>
2020-08-14 20:57:43 -0700

    madhu class library

--- Znalytics.Group5.Entities/Class1.cs
+++ Znalytics.Group5.Entities/Class1.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Znalytics.Group5.Entities
+﻿public class Airline
 {
-    public class Class1
+    private int _FlightNumber;
+    private string _View;
+    private int _Time;
+    private int _Date;
+    private string _Cancel;
+    public void SetFlightNumber(int value)
     {
+        _FlightNumber = value;
     }
+    public int FlightNumber()
+    {
+        return _FlightNumber;
+    }
+    public void SetView(string value)
+    {
+        _View = value;
+
+    }
+    public string GetView()
+    {
+        return _View;
+    }
+    public void SetTime(int value)
+    {
+        _Time = value;
+
+    }
+    public int GetTime()
+    {
+        return _Time;
+    }
+    public void SetDate(int value)
+    {
+        _Date = value;
+
+    }
+    public int GetDate()
+    {
+        return _Date;
+    }
+    public void SetCancel(string value)
+    {
+        _Cancel = value;
+
+    }
+    public string GetCancel()
+    {
+        return _Cancel;
+    }
+
 }
