78da31604414502eed1bdd2368d8c9209438026e
naddunurireshma <naddunurireshma@gmail.com>
2020-08-15 13:24:52 +0530

    reshma

---
+++ Znalytics.Group5.Entities/Flightsearching.cs
@@ -0,0 +1,41 @@
+﻿public class FlightSearching
+{
+    private int _NoOfFlights;
+    private string _FlightName;
+    private int _FlightDate;
+    private int _Time;
+    public void SetNoOfFlight(int value)
+    {
+        _NoOfFlights = value;
+    }
+    public int GetNoOfFlight()
+    {
+        return _NoOfFlights;
+    }
+    public void SetFlightName(string value)
+    {
+        _FlightName = value;
+    }
+    public string GetFlightName()
+    {
+        return _FlightName;
+    }
+    public void SetFlightDate(int value)
+    {
+        _FlightDate = value;
+
+    }
+    public int GetFlightDate()
+    {
+        return _FlightDate;
+    }
+    public void SetTime(int value)
+    {
+        _Time = value;
+
+    }
+    public int GetTime()
+    {
+        return _Time;
+    }
+}
