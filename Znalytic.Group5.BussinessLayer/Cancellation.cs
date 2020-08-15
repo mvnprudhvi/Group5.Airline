3975405d3bb16e0d46fe794e88ebd5534cee8975
Madhumitha-Varma <madhumithagorla@gmail.com>
2020-08-15 01:52:16 -0700

    cancellation

--- Znalytic.Group5.BussinessLayer/Cancellation.cs
+++ Znalytic.Group5.BussinessLayer/Cancellation.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Znalytic.Group5.BussinessLayer
+﻿class Cancellation
 {
-    class Cancellation
-    {
-    }
+
 }
--- Znalytics.Group5.DataAccessLayer/Cancellation.cs
+++ Znalytics.Group5.DataAccessLayer/Cancellation.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Znalytics.Group5.DataAccessLayer
+﻿class DataAccessLayer
 {
-    class Cancellation
-    {
-    }
+
 }
