14a3e3cd9184cf93162035165611e53ec5103bd8
VanchnagiriDineshKumar <vanchanagiri.dinesh@gmail.com>
2020-08-16 21:21:55 +0530

    hii

--- Znalytic.Group5.BussinessLayer/Prices.cs
+++ Znalytic.Group5.BussinessLayer/Prices.cs
@@ -1,4 +1,16 @@
-﻿public class Airlineprices
-{ 
+﻿/// <summary>
+/// Airline prices
+/// </summary>
+public class Airlineprices
+{
+    private double _price;
 
+    public void SetPrice(double value)
+    {
+        _price = value;
+    }
+    public double Getprice()
+    {
+        return _price;
+    }
 }
--- Znalytics.Group5.Entities/Prices.cs
+++ Znalytics.Group5.Entities/Prices.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Znalytics.Group5.Entities
-{
-    class Prices
-    {
-    }
-}
+﻿
