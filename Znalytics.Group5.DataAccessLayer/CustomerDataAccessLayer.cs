909e21fecf3071babef771e1309101fb6c5e3432
mvnprudhvi <mvnprudhvi7@gmail.com>
2020-08-18 22:37:13 +0530

    hi

--- Znalytics.Group5.DataAccessLayer/CustomerDataAccessLayer.cs
+++ Znalytics.Group5.DataAccessLayer/CustomerDataAccessLayer.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections.Generic;
 using Znalytics.Group5.Airline.Entities;
 
-namespace Znalytics.Group5.Airline.CustomerDataAccessLayer
+namespace Znalytics.Group5.Airline.DataAccessLayer
 {
-    public class Customer : Customer
+    public class CustomerDataAccessLayer
     {
         //create list
-      List<Customer> Customer = new List<Customer>();
+        public class customer
+        {
+            List<Customer> Customer = new List<Customer>();
+        }
 
         public void AddCustomer(Customer Customer)
         {
             ////code generating new customer id
 
             //customers.Add(customer);
-
+            customer.Add()
             //not done
+
         }
     }
 }
