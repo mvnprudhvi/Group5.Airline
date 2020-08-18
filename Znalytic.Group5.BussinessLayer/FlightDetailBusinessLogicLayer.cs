f7b42ef3449eb5b9cb40b3c20f1bd9a433dd38e1
@keerthanaMudimadula <keerthana0562@gmail.com>
2020-08-18 10:38:10 -0700

    cmmitt

---
+++ Znalytic.Group5.BussinessLayer/FlightDetailBusinessLogicLayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Znalytic.Group5.BussinessLayer
+{
+    class FlightDetailBusinessLogicLayer
+    {
+        private static void AddFlight()
+        {
+            Flight flight = new Flight();
+            Console.Write("Enter flight name to be added: ");
+            /* string userinput = Console.ReadLine();
+             if(userinput != "null")
+             {
+                 flight.Add(userinput);
+                 foreach (object obj in flight)
+                     Console.Write(obj.ToString() + " ");
+             }
+             else
+             {
+                 Console.Write("not allowed");
+             }
+            */
+        }
+    }
