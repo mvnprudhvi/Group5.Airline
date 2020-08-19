d1421aa98c5ccc87d730540187656507f11b08c5
VanchnagiriDineshKumar <vanchanagiri.dinesh@gmail.com>
2020-08-19 22:09:44 +0530

    tt

--- Znalytic.Group5.BussinessLayer/PriceBusinessLogic.cs
+++ Znalytic.Group5.BussinessLayer/PriceBusinessLogic.cs
@@ -1,9 +1,14 @@
-﻿using Znalytics.Group5.Airline.Entities;
+﻿using Znalytics.Group5.Airline.DataAccessLayer;
+using Znalytics.Group5.Airline.Entities;
 
 namespace Znalytics.Group5.Airline.BusinessLogicLayer
 {
      public class PriceBusinessLogic
      {
-          
-     }
+        private PriceDataAccessLayer priceDataAccessLayer=new PriceDataAccessLayer();
+        public void AddPrice(double price)
+        {
+            priceDataAccessLayer.AddPrice(price);
+        }
+    }
 }
---
+++ Znalytics.Group5.Airline/PriceMenu.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+using Znalytics.Group5.Airline.BusinessLogicLayer;
+
+namespace Znalytics.Group5.Airline
+{
+    public class PriceMenu
+    {
+        private static PriceBusinessLogic priceBusinessLogic = new PriceBusinessLogic();
+        public static void Menu()
+        {
+            int choice = -1;
+
+            do
+            {
+                  WriteLine("Prices Menu:");
+                  WriteLine("1. Add Price");
+                  WriteLine("2. Update Price");
+                  WriteLine("9. Exit");
+
+                bool b = int.TryParse(ReadLine(), out choice);
+                if (b == true)
+                {
+                    switch (choice)
+                    {
+                        case 1: AddPrice(); break;
+                    }
+                }
+            } while (choice != 9);
+        }
+
+        public static void AddPrice()
+        {
+            Write("Enter the price:");
+            double price = double.Parse(ReadLine());
+            priceBusinessLogic.AddPrice(price);
+        }
+    }
+}
