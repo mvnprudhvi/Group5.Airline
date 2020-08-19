27a4d0005f26c45393c2f17e097702a94a3ddfba
Madhumitha-Varma <madhumithagorla@gmail.com>
2020-08-18 22:01:06 -0700

    madhu

---
+++ Znalytics.Group5.DataAccessLayer/CancellationDataAcessLayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Znalytics.Group5.AirLine.CancellationModule.Entities;
+
+namespace Znalytics.Group5.AirLine.DataAccessLayer
+{
+    public class FlightNameDataAccessLayer : FlightDataAccessLayer
+    {
+        //create list
+        List<Flightname> FlightName = new List<FlightName>();
+
+        public void AddFlightName(FlightName FlightName)
+        {
+            ////code generating new customer id
+
+            //customers.Add(customer);
+
+            //not done
+        }
+    }
+}
