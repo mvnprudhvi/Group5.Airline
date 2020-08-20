abe6b4b224baeff354433e8b74ea33fb5007531a
naddunurireshma <naddunurireshma@gmail.com>
2020-08-20 19:57:15 +0530

    reshma

--- Znalytics.Group5.Entities/Flightsearching.cs
+++ Znalytics.Group5.Entities/Flightsearching.cs
@@ -1,103 +1,125 @@
 ﻿// created by Reshma//
 /// <summary>
-/// Represents FlightSearching
+/// Represents Flightsearching
 /// </summary>
-public class FlightSearching
+   namespace Znaltyics.Group5.Airline.Flightsearching.Entities
 {
-    //private fields
-    private string _FlightName;
-    private int _NumberOfFlights;
-    private System.DateTime _FlightTime;
-    private System.DateTime _FlightDate;
-
-    /// <summary>
-    /// Constructor that initializes details of FlightSearching
-    /// </summary>
-    /// <param name="FlightName">Represents flight number</param>
-    ///     <param name=" NumberOfFlights">Represents no flight number</param>
-    /// <param name="FlightTime">represents time of flight</param>
-    /// <param name="FlightDate">represents date of flight</param>
-
-
-    public FlightSearching(string FlightName, int NumberOfFlights, int FlightTime, int FlightDate)
-    {
-        //_FlightName = flightName;
-        //_NumberOfFlights=numberoOfFlights
-        //_FlightDate = flightdate;
-        //_FlightTime =flighttime;
-
-
-
-        _FlightName = FlightName; //set method will be called
-        _NumberOfFlights = NumberOfFlights;
-        _FlightDate = FlightDate; //set method will be called
-        _FlightTime = FlightTime;
-
-    }
-
-    /// <summary>
-    /// Parameterless constructor
-    /// </summary>
-    public FlightSearching()
-    {
-        //not initializing
-    }
-
-
    
-    /// <summary>
-    /// Represents name of the AirLine
-    /// </summary>
-    public string FlightName
-    {
-        set
+        public class Flightsearching
         {
-            //Name should be less than 30 string characters
-            if (value.Length <= 30)
+            //auto-implimented properties
+            public int FlightID { get; set; }
+            public string FlightName { get; set; }
+
+        //public Flight(int FlightID, string FlightName)
+        //{
+        //    this.FlightID = FlightID;
+        //    this.FlightName = FlightName;
+        //}
+    }
+    
+
+
+
+
+     // public class FlightSearching
+    {
+        //private fields
+        private string _FlightName;
+        private int _NumberOfFlights;
+        private System.DateTime _FlightTime;
+        private System.DateTime _FlightDate;
+
+        /// <summary>
+        /// Constructor that initializes details of FlightSearching
+        /// </summary>
+        /// <param name="FlightName">Represents flight number</param>
+        ///     <param name=" NumberOfFlights">Represents no flight number</param>
+        /// <param name="FlightTime">represents time of flight</param>
+        /// <param name="FlightDate">represents date of flight</param>
+
+
+        public FlightSearching(string FlightName, int NumberOfFlights, int FlightTime, int FlightDate)
+        {
+            //_FlightName = flightName;
+            //_NumberOfFlights=numberoOfFlights
+            //_FlightDate = flightdate;
+            //_FlightTime =flighttime;
+
+
+
+            _FlightName = FlightName; //set method will be called
+            _NumberOfFlights = NumberOfFlights;
+            _FlightDate = FlightDate; //set method will be called
+            _FlightTime = FlightTime;
+
+        }
+    //
+
+        /// <summary>
+        /// Parameterless constructor
+        /// </summary>
+        public FlightSearching()
+        {
+            //not initializing
+        }
+
+
+
+        /// <summary>
+        /// Represents name of the AirLine
+        /// </summary>
+        public string FlightName
+        {
+            set
             {
-                _FlightName = value;
+                //Name should be less than 30 string characters
+                if (value.Length <= 30)
+                {
+                    _FlightName = value;
+                }
+            }
+
+            get
+            {
+                return _FlightName;
             }
         }
 
-        get
+        /// <summary>
+        /// date of flight
+        /// </summary>
+        /// 
+
+
+        public System.DateTime date
         {
-            return _FlightName;
+            set
+            {
+                _Date = value;
+            }
+
+            get
+            {
+                return _Date;
+            }
+        }
+        //
+        public int Time
+        {
+            set
+            {
+
+                if ()
+                {
+                    _Time = value;
+                }
+            }
+
+            get
+            {
+                return _Time;
+            }
         }
     }
-
-    /// <summary>
-    /// date of flight
-    /// </summary>
-    /// 
-
-
-  public System.DateTime date
-    {
-        set
-        {
-            _Date = value;
-        }
-
-       get
-        {
-            return _Date;
-        }
-    }
-    //
-    public int Time
-{
-    set
-    {
-       
-        if ()
-        {
-            _Time = value;
-        }
-    }
-
-    get
-    {
-        return _Time;
-    }
-}
     //
