c1cf1fd659030f45d34d29b0c8fee5bc785d8555
naddunurireshma <naddunurireshma@gmail.com>
2020-08-21 13:18:45 +0530

    reshma

--- Znalytics.Group5.Entities/Flightbooking.cs
+++ Znalytics.Group5.Entities/Flightbooking.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Znalytics.Group5.Entities
+namespace Znalytics.Group5.Airline.Flightbooking.Entities
 {
     class Flightbooking
     {
+        //auto-implimented properties
+        public int NoOfFlights { get; set; }
+        private System.DateTime FlightTime { get; set; }
+        private System.DateDate FlightDate { get; set; }
 
 
 
-
+        //public Flight(int NoOfFlights,System.DateTime FlightTime System.DateDate FlightDate )
+        //{
+        //    this.NoOfflights = NoOfflights;
+        //    this.FlightTime = FlightTime;
+        //}
     }
+
+
+
 }
+
