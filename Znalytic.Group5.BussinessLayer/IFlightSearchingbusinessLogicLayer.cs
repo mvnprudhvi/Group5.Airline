00d66c7743d5629f55d0c964615bc15451d52e9c
naddunurireshma <naddunurireshma@gmail.com>
2020-08-26 19:16:51 +0530

    rrrrrrr

---
+++ Znalytic.Group5.BussinessLayer/IFlightSearchingbusinessLogicLayer.cs
@@ -0,0 +1,11 @@
+﻿//////using System;
+namespace Znalytics.Group5.Airline.FlightSearchingbussinessLogicLayer
+{
+    public interface IFlightSearchingbusinessLogicLayer
+    {
+        public void Add(FlightName FlightName)
+
+
+        public void UpdateFlightName(FlightName FlightName)
+    }
+}
