12e052efd29f3dfc2519ee0b8c55932e7e3bc109
naddunurireshma <naddunurireshma@gmail.com>
2020-08-26 22:11:49 +0530

    rrrrrrrr

--- Znalytics.Group5.Airline/FlightBookingPL.cs
+++ Znalytics.Group5.Airline/FlightBookingPL.cs
@@ -23,24 +23,92 @@ namespace  FlightBooking
 
                 Console.Write("enter passengerName:");
                 string passengerName = Console.ReadLine();
+                    Console.Write("enter booking id:");
+                 int bookingId = int.Parse(Console.ReadLine());
 
-                //Random class generates pseudo Randon numbers
-                var random = new Random();
+            //Random class generates pseudo Randon numbers
+            var random = new Random();
                 int RandomNo = random.Next();
                 int SeatNumber = RandomNo;
 
-                Console.WriteLine("================== FlightBooking ==========================");
+          
+            Console.WriteLine("================== FlightBooking ==========================");
                 Console.WriteLine(" flightName :" + flightName);
                 Console.WriteLine(" flightId :" + flightId);
                 Console.WriteLine(" passengerName :" + passengerName);
+                Console.WriteLine("bookingId:" + bookingId);
                 Console.WriteLine(" you are alloted in seat number: " + SeatNumber);
                 Console.WriteLine("============================================================");
 
                 Console.WriteLine(" flightName   flightId  passengerName Seat Number");
                 Console.WriteLine("-----------------------------------------------------");
 
+
+            static void FlightBookingMenu()
+            {
+                int choice = 1;
+                do
+                {
+                    Console.WriteLine("========FLIGHTBOOKING MENU=========");
+                    Console.WriteLine("1. Add FlightBooking");
+                    Console.WriteLine("2. Update FlightBooking");
+                    Console.WriteLine("3. View FlightBooking");
+                    Console.WriteLine("4. Delete FlightBooking");
+                    Console.WriteLine("5. Exit");
+                    Console.Write("Enter choice: ");
+                    choice = int.Parse(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1: AddNewFlightBooking(); break;
+                        case 2: ViewFlightBooking(); break;
+                        case 3: UpdateFlightBooking(); break;
+                        case 4: DeleteFlightBooking(); break;
+
+                    }
+
+                } while (choice != 5);
+            }
+
+            static void AddNewFlightBooking()
+            {
+
+                //create another list in the collection called  "flightbooking1"
+                List<FlightBooking> flightBooking1 = new List<FlightBooking>() {
+                 new FlightBooking() {  flightName = "AirInia", flightNumber = "101", source = "Hyderabad", destination = "Mumbai", departureTiming = "2020-08-29 7:30:00 AM", arrivalTiming = " 2020-08-29 7:30:00 Am",SeatNumber = "01",bookingId ="1234"},
+                 new FlightBooking() { flightName = "AirIndia", flightNumber = "101",  source = "Hyderabad", destination = "Mumbai", departureTiming = "2020-08-29 7:30:00 AM", arrivalTiming = " 2020-08-29 7:30:00 Am",seatNumber = "02",bookingId ="1234"},
+                 new FlightBooking() { flightName = "AirIndia", flightNumber = "101",   source = "Hyderabad", destination = "Mumbai", departureTiming = "2020-08-29 7:30:00 AM", arrivalTiming = " 2020-08-29 7:30:00 Am",SeatNumber = "03",bookingId ="3456" },
+                 new FlightBooking() {  flightName = "AirIndia", flightNumber = "101",   source = "Hyderabad", destination = "Mumbai", departureTiming = "2020-08-29 7:30:00 AM", arrivalTiming = " 2020-08-29 7:30:00 Am",SeatNumber = "04",bookingId ="7891"},
+                 new FlightBooking() {  flightName = "AirIndia", flightNumber = "101",   source = "Hyderabad", destination = "Mumbai", departureTiming = "2020-08-29 7:30:00 AM", arrivalTiming = " 2020-08-29 7:30:00 Am",SeatNumber = "05",bookingId ="6789"}
+                 };
+
+            // add all the elements of "flightSchedule1" collection at the end of "flightschedule" colletion
+            FlightBooking.AddRange(flight Booking);
+
+                //get values from list collection
+                foreach (FlightBooking item in FlightBooking)
+                {
+                    Console.WriteLine(item);
+
+
+                }
+            Console.ReadKey();
+        }
+
+        static void ViewFlightBooking()
+        {
+
+            List< FlightBooking fb = FlightBookingbusinessLogic.GetFlightBooking();
+            FlightBooking fb = new FlightBooking();
+
+            foreach (FlightBooking item in fb)
+            {
+                Console.WriteLine(fb.flightName + "," + fb.flightId + "," +   fb.source + "," + fb.destination + "," + fb.arrivalTiming + "," + fb+.departureTiming+ "," + fb.seatNmuber + ","+ fb.bookingId+",");
+
             }
         }
+
     }
+}
 
     
