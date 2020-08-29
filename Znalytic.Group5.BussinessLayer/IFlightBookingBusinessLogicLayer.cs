82e2d38b3cf3c9b4067ed639d50094b077648c90
naddunurireshma <naddunurireshma@gmail.com>
2020-08-29 20:03:38 +0530

    rr

--- Znalytic.Group5.BussinessLayer/IFlightBookingBusinessLogicLayer.cs
+++ Znalytic.Group5.BussinessLayer/IFlightBookingBusinessLogicLayer.cs
@@ -2,5 +2,17 @@
 {
     public interface IFlightBookingBusinessLogicLayer
     {
+        /this method represents add FlightBooking
+        public void FlightBooking(FlightBooking FlightBooking);
+
+        //this method represents update FinglightBooking
+        public void UpdateFlightBooking(FlightBooking FlightBooking);
+
+        //this method represents get FlightBooking by customer id
+        public FlightBooking GetFlightBookingByCustomerID(int _customerID);
+
+        //this method represents delete FlightBooking
+        public void DeleteFlightBooking(FlightBooking fb);
     }
+}
 }
