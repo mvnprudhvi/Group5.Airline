667a0bd11e59094c17432f7be2a049fdd62f3dd1
naddunurireshma <naddunurireshma@gmail.com>
2020-08-29 16:28:46 +0530

    rr

--- Znalytics.Group5.Airline/FlightBookingbusinessLogic.cs
+++ Znalytics.Group5.Airline/FlightBookingbusinessLogic.cs
@@ -14,5 +14,20 @@ namespace Znalytics.Group5.Airline.PresentationLayer
         {
             throw new NotImplementedException();
         }
+
+        internal void AddFlightBooking(FlightBooking fb)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void UpdateFlightBooking(FlightBooking fb)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal System.Collections.Generic.List<FlightBooking> GetFlightBooking()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
