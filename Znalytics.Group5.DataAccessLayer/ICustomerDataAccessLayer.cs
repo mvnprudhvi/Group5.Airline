4741f2d70d8b49918b3b68df08aa7abe3e2d20de
mvnprudhvi <mvnprudhvi7@gmail.com>
2020-08-29 15:41:00 +0530

    error

---
+++ Znalytics.Group5.DataAccessLayer/ICustomerDataAccessLayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Znalytics.Group5.Airline.Entities;
+
+namespace Znalytics.Group5.Airline.DataAccessLayer
+{
+    interface ICustomerDataAccessLayer
+    {
+        void AddCustomer(Customer customer);
+        List<Customer> GetCustomer();
+        void UpdateCustomer(Customer customer);
+        void DeleteCustomer(Customer customer);
+       Customer GetCustomerByCustomerId(string CustomerId);
+        Customer GetCustomerByCustomerUserName(string CustomerUserName);
+
+
+
+    }
+}
