577be607c036068906282184ad66e151e2558b37
Madhumitha-Varma <madhumithagorla@gmail.com>
2020-08-30 20:46:48 -0700

    ..

--- Znalytics.Group5.DataAccessLayer/ITicketCancellationDataAccessLayer.cs
+++ Znalytics.Group5.DataAccessLayer/ITicketCancellationDataAccessLayer.cs
@@ -1,13 +1,26 @@
 ﻿//created by Madhumitha.....
+using System.Collections.Generic;
 using Znalytics.Group5.Airline.Entities;
 
 namespace Znalytics.Group5.DataAccessLayer
 {
     interface ITicketCancellationDataAccessLayer
     {
-        //
-        public void AddTicketCancellations(TicketCancellation bookingId);
+        //method to add ticket cancellations to the list
 
+        void AddTicketCancellations(TicketCancellation bookingId);
 
+        //method to get ticket cancellations by cancellation Id
+
+        List<TicketCancellation> GetTicketCancellationsByCancellationID(int cancellationID);
+
+        //method Get TicketCancellations By Customer ID
+        List<TicketCancellation> GetTicketCancellationsByCustomerID(int customerID);
+
+        //method Get Ticket Cancellations By Booking ID
+        List<TicketCancellation> GetTicketCancellationsByBookingID(int bookingID);
+
+        //method to get ticket cancellations
+        List<TicketCancellation> GetTicketCancellations();
     }
 }
