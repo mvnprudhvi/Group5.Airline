7ba154bf42471ab52eebce21f2b96bd16f6bf649
mvnprudhvi <mvnprudhvi7@gmail.com>
2020-08-31 14:32:14 +0530

    json

--- Znalytics.Group5.DataAccessLayer/CustomerDataAccessLayer.cs
+++ Znalytics.Group5.DataAccessLayer/CustomerDataAccessLayer.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using Znalytics.Group5.Airline.Entities;
 using Znalytics.Group5.Airline.DataAccessLayer;
 using Znalytics.Group5.AirLine.Entities;
+using Newtonsoft.Json;
+using System.IO;
 
 //Created a Namespace For DataAccess Layer For Customer Module
 namespace Znalytics.Group5.Airline.DataAccessLayer
@@ -26,6 +28,36 @@ namespace Znalytics.Group5.Airline.DataAccessLayer
     static CustomerDataAccessLayer()
         {
             _customer = new List<Customer>();
+        }
+
+        /// <summary>
+        /// Saving the data into Json file
+        /// </summary>
+        private static void SaveIntoFile()
+        {
+            //Serialization converts an object into Json Format/String
+            //Serialize object is stored in a reference variable of a string
+            string s = JsonConvert.SerializeObject(_customer);
+
+            //Strean Writer writes data into file.
+            StreamWriter streamWriter = new StreamWriter(@"C:\Users\Administrator\Desktop\Customer.txt");
+            streamWriter.Write(s);
+            streamWriter.Close();
+        }
+        /// <summary>
+        /// reading the data from Json file and return the data in the file in List format
+        /// </summary>
+        /// <returns>Returns List of flightdetails  avaliable int Flight.Txt</returns>
+        public List<Customer> GetFiledata()
+        {
+            //Stream Reader reads the data from the given file
+            StreamReader streamReader = new StreamReader(@"C:\Users\Administrator\Desktop\Customer.txt");
+            string str = streamReader.ReadToEnd();
+
+            //Deserialization converts Json data/string to Object
+            List<Customer> customer = JsonConvert.DeserializeObject<List<Customer>>(str);
+            return customer;
+
         }
 
         /// <summary>
