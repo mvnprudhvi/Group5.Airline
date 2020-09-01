57e3d6ed7d46086035f046677faf081fe3a6aae9
VanchnagiriDineshKumar <vanchanagiri.dinesh@gmail.com>
2020-09-01 09:07:25 +0530

    rr

--- Znalytics.Group5.DataAccessLayer/FlightPriceDataAccessLayer.cs
+++ Znalytics.Group5.DataAccessLayer/FlightPriceDataAccessLayer.cs
@@ -1,64 +1,142 @@
 ﻿/// <summary>
 /// Created By Dinesh
-/// Module Name is Flight Price
+/// Module Name is FlightPrice
 /// </summary>
 
+//This Represents Importing Statements
 using System.Collections.Generic;
 using Znalytics.Group5.Airline.Entities;
+using Newtonsoft.Json;
+using System.IO;
 
+/// <summary>
+/// Created a namespace for Data Access Layer of Airline Reservation System
+/// </summary>
 namespace Znalytics.Group5.Airline.DataAccessLayer
 {
     /// <summary>
-    /// This Class Represents Data Access Layer Of Price
+    /// This Class Represents Data Access Layer Of FlightPrice
     /// </summary>
     public class FlightPriceDataAccessLayer : IFlightPriceDataAccessLayer
     {
-        //private Field of price Collection
+        //creating static list
         private static List<FlightPrice> _flightPrices;
 
-        //constructor for  Data Access Layer
+        /// <summary>
+        ///static constructor for  Data Access Layer
+        ///Creating List to allocate the Memory only Once
+        /// </summary>
         static FlightPriceDataAccessLayer()
         {
+            //The Object of List class is stored in reference variable
             _flightPrices = new List<FlightPrice>();
         }
 
         /// <summary>
-        /// This Method Represents AddPrice
+        /// Saving the data into Json file
+        /// </summary>
+        private static void SaveIntoFile()
+        {
+            // Serialization converts an object into Json Format/ String
+            //Serialize object is stored in a reference variable of a string
+            string s = JsonConvert.SerializeObject(_flightPrices);
+
+            //Strean Writer writes data into file.
+            StreamWriter streamWriter = new StreamWriter(@"C:\FlightPricesList.txt");
+            streamWriter.Write(s);
+            streamWriter.Close();
+        }
+
+        /// <summary>
+        /// reading the data from Json file and return the data in the file in List format
+        /// <returns>Returns List of warehouses avaliable int FlightPricesList.Txt</returns>
+        /// </summary>
+        public static List<FlightPrice> GetFileData()
+        {
+            //Stream Reader reads the data from the given file
+            StreamReader streamReader = new StreamReader(@"C:\FlightPricesList.txt");
+            string s1 = streamReader.ReadToEnd();
+            //Deserialization converts Json data/string to Object
+            List<FlightPrice> fligthprice = JsonConvert.DeserializeObject<List<FlightPrice>>(s1);
+            streamReader.Close();
+            return fligthprice;
+        }
+
+
+        /// <summary>
+        /// This Method Represents AddPrice to Add the FlightPrice Details into List
         /// </summary>
         /// <param name="price"></param>
         public void AddFlightPrice(FlightPrice price)
         {
             _flightPrices.Add(price);
+            SaveIntoFile();
         }
 
         /// <summary>
-        /// This Method Repreents Delete Price
+        /// This Method Represents DeletePrice to Delete the FlightPrice Details into List
         /// </summary>
         /// <param name="price"></param>
         public void DeleteFlightPrice(FlightPrice price)
         {
-            //Based on  ScheduleID the Price Will be deleted 
+            //Finding the ScheduleId Which Existing in List or not 
             FlightPrice dpri = _flightPrices.Find(temp => temp.FlightScheduleId == price.FlightScheduleId);
+
+            //Based on  ScheduleID the Flight Price Will be Remove(Deleted)
             _flightPrices.Remove(dpri);
+            SaveIntoFile();
         }
 
         /// <summary>
-        /// This Method Represents UpdatePrice
+        /// This Method Represents UpdatePrice to Update the FlightPrice Details into List
         /// </summary>
         /// <param name="price"></param>
         public void UpdateFlightPrice(FlightPrice price)
         {
-            //Based on ScheduleID the Price Will be Updated 
+            //Finding the ScheduleId Which Existing in List or not
             FlightPrice upri = _flightPrices.Find(temp => temp.FlightScheduleId == price.FlightScheduleId);
+
+            //Based on the ScheduleId  Business Class seat Price Will be Updating
             upri.PriceForBusinessClassSeat = price.PriceForBusinessClassSeat;
+
+            //Based on the ScheduleId  Business Class seat Price Will be Updating
             upri.PriceForEconomyClassSeat = price.PriceForEconomyClassSeat;
+            SaveIntoFile();
         }
 
         /// <summary>
-        /// This Method Represents GetPrices
+        /// This Method Represents tO Update the Weekend Hike Percentage Based on Single ScheduleId
         /// </summary>
-        /// <returns>_prices</returns>
-        public List<FlightPrice> GetFlightPrices()
+        /// <param name="price"></param>
+        public void UpdateWeekendPriceHikePercentageByScheduleId(FlightPrice price)
+        {
+            //Finding the ScheduleId Which Existing in List or not
+            FlightPrice uwpri = _flightPrices.Find(temp => temp.FlightScheduleId == price.FlightScheduleId);
+
+            //Based on ScheduleId Weekend Price Hike Percentage Will be Updated
+            uwpri.WeekendPriceHikePercentage = price.WeekendPriceHikePercentage;
+            SaveIntoFile();
+        }
+
+        /// <summary>
+        /// This Method Represents to Update the Weekend Hike Percentage For All ScheduleId's
+        /// </summary>
+        /// <param name="weekendPriceHikePercentage"></param>
+        public void UpdateWeekendPriceHikePercentageForAllSchedules(double weekendPriceHikePercentage)
+        {
+            //Updating Hike For all Schedule Id's
+            foreach (FlightPrice pri in _flightPrices)
+            {
+                pri.WeekendPriceHikePercentage = weekendPriceHikePercentage;
+            }
+            SaveIntoFile();
+        }
+
+        /// <summary>
+        /// This Method Represents to Get the FlightPrices Based on Before Days
+        /// </summary>
+        /// <returns>_priceDataAccessLayer</returns>
+        public List<FlightPrice> GetFlightPricesByBeforeDays()
         {
             return _flightPrices;
         }
--- Znalytics.Group5.DataAccessLayer/IFlightPriceDataAccessLayer.cs
+++ Znalytics.Group5.DataAccessLayer/IFlightPriceDataAccessLayer.cs
@@ -1,36 +1,61 @@
-﻿
+﻿// <summary>
+/// Created By Dinesh
+/// Module Name is FlightPrice
+/// </summary>
+
+//Importing Statements
 using System.Collections.Generic;
 using Znalytics.Group5.Airline.Entities;
 
+/// <summary>
+/// Created a namespace for Data Access Layer of Airline Reservation System
+/// </summary>
 namespace Znalytics.Group5.Airline.DataAccessLayer
 {
     /// <summary>
-    ///  Interface For Flight Price Data Access Layer Consists of Following Methods to implement
+    ///  This Represents an Interface of FlightPrice Data Access Layer 
+    ///  It Consists of Following Methods to implement Compulsory
     /// </summary>
     public interface IFlightPriceDataAccessLayer
     {
         /// <summary>
-        /// This Represents Declaration of Add Flight Price Method
+        /// This Represents Method Declaration of Add FlightPrice to Add the FlightPrice
         /// </summary>
-        /// <param name="price"></param>
+        /// <param name="price">Represents the object of FlightPrice</param>
         void AddFlightPrice(FlightPrice price);
 
+
         /// <summary>
-        /// This Represents Declaration of Delete Flight Price Method
+        /// This Represents Method Declaration of Delete FlightPrice to Delete the FlightPrice
         /// </summary>
-        /// <param name="price"></param>
+        /// <param name="price">Represents the object of FlightPrice</param>
         void DeleteFlightPrice(FlightPrice price);
 
         /// <summary>
-        /// This Represents Declaration of Update Flight Price Method
+        /// This Represents Method Declaration of Update FlightPrice  to Update the FlightPrice 
         /// </summary>
-        /// <param name="price"></param>
+        /// <param name="price">Represents the object of FlightPrice</param>
         void UpdateFlightPrice(FlightPrice price);
 
+
         /// <summary>
-        /// This Represents Declaration of Get Flight Price Method
+        /// This Represents Method Declaration of Get FlightPrices Based on Beforedays
         /// </summary>
-        /// <param name="price"></param>
-        List<FlightPrice> GetFlightPrices();
+        List<FlightPrice> GetFlightPricesByBeforeDays();
+
+
+
+        /// <summary>
+        /// This Represents Method Declaration of Update Weekend Hike Percentage Based On ScheduleId  
+        /// </summary>
+        /// <param name="price">Represents the object of FlightPrice</param>
+        void UpdateWeekendPriceHikePercentageByScheduleId(FlightPrice price);
+
+
+        /// <summary>
+        /// This Represents Method Declaration of Update Weekend Hike Percentage For All ScheduleId's
+        /// </summary>
+        /// <param name="weekendPriceHikePercentage"></param>
+        void UpdateWeekendPriceHikePercentageForAllSchedules(double weekendPriceHikePercentage);
     }
 }
